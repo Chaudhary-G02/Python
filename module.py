7618763711124b78412cd8c928709aa785bdbc94
Krish Chaudhary <chaudharykrish912@gmail.com>
2025-10-29 19:20:34 +0530

    learned modules

---
+++ module.py
@@ -0,0 +1,5 @@
+def say_hello():
+    print("Hello from mymodule!")
+
+def add_numbers(a, b):
+    return a + b
---
+++ module2.py
@@ -0,0 +1,5 @@
+import module
+
+module.say_hello()
+result = module.add_numbers(5, 7)
+print("The sum is:", result)
